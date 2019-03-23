727bdb0216c90c206c6ea4a74628036f58bc6578
Mark Jarvis <43512302+jarvisms@users.noreply.github.com>
2019-03-23 22:32:37 +0000

    Mainly Documentation
    Written all documentation, but also some PEP8 tweaks and use of NamedTuples for readings.

--- README.md
+++ README.md
@@ -1,1 +1,117 @@
 # PythonUM24C
+
+This Python module provides a class and methods for interfacing with the RDTech UM24C USB Meter with Bluetooth connectivity. This library has been based on predominantly on the information provided [here](https://sigrok.org/wiki/RDTech_UM_series). This has been written in Python 3.7.2, but may work in earlier versions but this is not guaranteed.
+
+## Prerequisites
+
+The [`pyserial`](https://pypi.org/project/pyserial/) module is needed to allow the use of the Bluetooth Serial Port interface to connect to the device.
+```
+pip install pyserial
+```
+
+## Installing
+There is no need to clone this repo since the module is a single file. You can simply download the ['um24c.py'](um24c.py) file to a location within the import path of your project, and then simply import it.
+
+## Usage
+
+Once imported, you may connect to the device in two ways;
+- Using Python's built in sockets interface - this relies on the underlying operating system compatibility however and knowing the MAC Address of the device directly
+- Using the legacy Bluetooth Serial Port interface - this relies on the user bonding the device and creating a Bluetooth virtual serial port via other means, such as with operating system utilities.
+
+Using Direct Socket interface
+```
+from um24c import UM24C
+meter  =  UM24C("01:23:45:67:89:AB")
+```
+OR...
+
+Using BT VSP interface where "False" denotes that sockets should not be used.
+```
+from um24c import UM24C
+meter  =  UM24C("COM21",False)
+```
+
+After you have created your connected instance, the various methods are then available to use - the main one being `get_reads` to read data, but others to remotely control the device.
+
+When finished, you may simply delete the object to disconnect, such as `del meter` or call `meter.close()` which disconnects but retains the object.
+
+If the connection seems to have been lost, you can call `meter.reset_port(device)` where `device` is the equivelent of the socket or VSP port from when it was initialised. However, in this case, the type of connection must be the same. i.e. you can use this to change from `COM21` to `COM22` for example, but not from `COM21` to `01:23:45:67:89:AB` as this would be a change of connection type.
+
+### Getting Readings
+`meter.get_reads()`
+Calling this method will request readings from the meter device, scale the readings and returns them as a Named Tuple as follows:
+```
+Reads(
+    timestamp = datetime.datetime(2019, 3, 23, 21, 33, 5, 1331),    # datetime (Python datetime.datetime naive object)
+    V = 5.34,         # Voltage (V)
+    A = 0.919,        # Amperage (A)
+    W = 4.907,        # Wattage (W)
+    C = 26,           # Temperature (°C)
+    F = 79,           # Temperature (°F)
+    Group = 3,        # Group Number (#)
+    mAh0 = 177,       # Capacity (mAh) for Group 0
+    mWh0 = 934,       # Energy (mWh) for Group 0
+    ...                 # Capacity and Energy for Groups 1-9 as above
+    Dpos = 1.33,      # USB D+ Voltage (V)
+    Dneg = 1.33,      # USB D- Voltage (V)
+    Mode = 0,         # Charge Mode (enum)
+    mAh = 1501,       # Record Capacity (mAh)
+    mWh = 8053,       # Record Energy (mWh)
+    At = 0.0,         # Record Threshold (A)
+    S = 5040,         # Record Duration (S)
+    Active = 0,       # Recording Active (True/False)
+    Screensaver = 9,  # Screen Timeout (min)
+    Backlight = 0,    # Backlight (#)
+    Ohms = 5.8,       # Resistance (Ω)
+    Screen = 0        # Screen (#)
+)
+```
+Since this is a Named Tuple, you may retrieve individual parameters directly by its attribute name, or by an index number:
+```
+result = meter.get_reads()
+result.V    # Gives 5.34 in example above
+result[2]   # Corresponds to A, and would give 0.919 in example above
+```
+
+### Device Control Functions
+The UM24C can also be remotely controlled to some extent - although there's not a great deal te be controlled. They are summarised here:
+
+`meter.next_screen()`
+Changes to the next screen mode on the device. Equivalent of pressing the next button.
+
+`meter.rotate_screen()`
+Rotates the screen orientation on the device. Equivalent of pressing and holding the top right button.
+
+`meter.next_data_group()`
+Changes to the next data storage group on the device. Equivalent to pressing and holding the bottom right button on screen 1.
+
+`meter.clear_data_group()`
+Clears the current data storage group on the device. Equivalent to pressing and holding the bottom left button on screen 1.
+
+`meter.set_threshold(f)`
+Sets the recording theshold to `f`, between 0A and 0.3A. Equivalent to changing the settings on screen 3.
+
+`meter.set_backlight(i)`
+Changes the backlights settings to `i` between 0 and 5. Equivalent to changing the settings on screen 7.
+
+`meter.set_screensaver(i)`
+Changes the number of minutes until the screensaver blanks the screen to `i` between 0 (never turn off) and up to 9 minutes. Equivalent to changing the settings on screen 7.
+
+## Author & Motivation
+
+**Mark Jarvis** - [jarvisms](https://github.com/jarvisms)
+
+I code in Python as a hobby and tinker with electronics. I bought one of these devices from eBay out of interest and decided to write a little library for it to allow it to be integrated into  - such as a logger.
+
+## Contributions & Feature requests
+
+For bugs, or feature requests, please contact me via GitHub or raise an issue.
+
+## License
+
+This project is licensed under the GNU General Public License v3.0 - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgements
+
+* Everything has been based on information found on [sigrok](https://sigrok.org/wiki/RDTech_UM_series)
+* This site also links to another Python library called [rdumtool](https://github.com/rfinnie/rdumtool), however my implementation is not based on this as it was written entirely from scratch
--- um24c.py
+++ um24c.py
@@ -2,41 +2,47 @@ import socket
 import serial
 import struct
 import operator
+from collections import namedtuple
 from datetime import datetime, timezone
 
 class btserial(serial.Serial):
+    """Provides send and recv methods to Serial class to be consistent with
+    socket classes"""
     def send(self,data):
         return self.write(data)
     def recv(self,num):
         return self.read(num)
 
 class UM24C:
+    """UM24C class provides methods for easily interfacing with the UM24C Meter"""
+    # Formats, keys and instruction bytes as per https://sigrok.org/wiki/RDTech_UM_series
     fmt = ">2x2HI2HxB20I2HBx2IHIBx2HIxB2x"
     conv = struct.Struct(fmt)
     keys = (
-        "Voltage (V)",      # Originally cV
-        "Amperage (A)",     # Originally cA
-        "Wattage (W)",     # Originally mW
-        "Temperature (°C)",
-        "Temperature (°F)",
-        "Group (#)",
-            *( "Group {}, {}".format(i,u)
+        "V",        # Voltage (V), Originally cV
+        "A",        # Amperage (A) Originally cA
+        "W",        # Wattage (W) Originally mW
+        "C",        # Temperature (°C)
+        "F",        # Temperature (°F)
+        "Group",    # Group (#)
+            *( "{}{}".format(u,i)
             for i in range(10)
-            for u in ("Capacity (mAh)", "Energy (mWh)")
+            for u in ("mAh", "mWh")     # Capacity (mAh), Energy (mWh)
             ),
-        "USB D+ Voltage (V)",  # Originally cV
-        "USB D- Voltage (V)",  # Originally cV
-        "Charge Mode (enum)",
-        "Record Capacity (mAh)",
-        "Record Energy (mWh)",
-        "Record Threshold (A)",    # Originally cA
-        "Record Duration (S)",
-        "Recording Active (?)",
-        "Screen Timeout (min)",
-        "Backlight (#)",
-        "Resistance (Ω)",  # Originally dΩ
-        "Screen (#)",
+        "Dpos",       # USB D+ Voltage (V) Originally cV
+        "Dneg",       # USB D- Voltage (V) Originally cV
+        "Mode",     # Charge Mode (enum)
+        "mAh",      # Record Capacity (mAh)
+        "mWh",      # Record Energy (mWh)
+        "At",       # Record Threshold (A)
+        "S",        # Record Duration (S)
+        "Active",   # Recording Active (?)
+        "Screensaver",  # Screen Timeout (min)
+        "Backlight",    # Backlight (#)
+        "Ohms",     # Resistance (Ω) Originally dΩ
+        "Screen",   # Screen (#)
     )
+    # Rescales particular items to standard units.
     scales = {
         0 : 0.01,   # cV to V
         1 : 0.001,  # cA to A
@@ -50,9 +56,18 @@ class UM24C:
         scales[i] if i in scales else 1     # Use Scale, else 1
         for i in range(len(keys))           # For each key
     )
-    multipliers = multipliers(scales,keys)  # execute the lambda to get around the scope issue with generators/comprehensions in  class variables
+    multipliers = multipliers(scales,keys)  # execute the lambda to get around the scope issue with generators/comprehensions in class variables
+    Reads = namedtuple("Reads", ('timestamp', *keys))
     def __init__(self, device, usesocket=True):
+        """Initialise connection with the hardware.
+        When 'usesocket' is True (default), 'device' should be the MAC address 
+        of the Bluetooth such as '01:23:45:67:89:AB'. If False, then the name
+        of the bluetooth serial port should be used for legacy mode connection
+        such as '/dev/rfcomm0', or 'COM20' where this has been mapped via the
+        Operating System's utilities.
+        """
         self.socket = usesocket
+        # Preferentially use BT Sockets, but also allow for Serial device
         if usesocket:
             self.port = socket.socket(
                 socket.AF_BLUETOOTH,
@@ -82,30 +97,30 @@ class UM24C:
         self.port.close()
     def close(self):
         self.port.close()
-    def ResetPort(self, device):
+    def reset_port(self, device):
+        """Attempts to close the port and reopen with the new device which 
+        must be of the same type as previous"""
         if hasattr(self, "port"):
             self.port.close()
         self.__init__(device,self.socket)
-    def GetReads(self, raw=False):
+    def get_reads(self):
         '''Gets readings from the UM24C device from the given serial port'''
         port = self.port
         i = 3   # Number of attempts to make
         while True:
             try:
                 buff = bytearray()
-                port.send(bytes([0xF0]))
+                port.send(bytes([0xF0]))    # The Get Readings instruction
                 while len(buff) < 130:
                     buff += port.recv(4096)
-                results = self.conv.unpack(buff[-130:])
-                if not raw:
-                    results = tuple(
-                        map(
-                            operator.mul,
-                            self.multipliers,
-                            results,
-                        )
+                results = tuple(
+                    map(
+                        operator.mul,
+                        self.multipliers,
+                        self.conv.unpack(buff[-130:]),
                     )
-                return (datetime.now(timezone.utc), *results)
+                )
+                return self.Reads(datetime.now(), *results)
                 break
             except (OSError, serial.SerialException, struct.error) as err:
                 print(err)
@@ -122,31 +137,41 @@ class UM24C:
                     raise
                     break
         return
-    def NextScreen(self):
+    def next_screen(self):
+        """Changes to the next screen mode on the device
+        Equivalent of pressing the next button"""
         try:
             self.port.send(bytes([0xF1]))
             print("Command Sent")
         except (OSError, serial.SerialException) as err:
             print(err)
-    def RotateScreen(self):
+    def rotate_screen(self):
+        """Rotates the screen orientation on the device
+        Equivalent of pressing and holding the top right button"""
         try:
             self.port.send(bytes([0xF2]))
             print("Command Sent")
         except (OSError, serial.SerialException) as err:
             print(err)
-    def NextDataGroup(self):
+    def next_data_group(self):
+        """Changes to the next data storage group on the device
+        Equivalent to pressing and holding the bottom right button on screen 1"""
         try:
             self.port.send(bytes([0xF3]))
             print("Command Sent")
         except (OSError, serial.SerialException) as err:
             print(err)
-    def ClearDataGroup(self):
+    def clear_data_group(self):
+        """Clears the current data storage group on the device
+        Equivalent to pressing and holding the bottom left button on screen 1"""
         try:
             self.port.send(bytes([0xF4]))
             print("Command Sent")
         except (OSError, serial.SerialException) as err:
             print(err)
-    def SetThreshold(self, threshold):
+    def set_threshold(self, threshold):
+        """Sets the recording threshold to 'threshold', between 0A and 0.3A
+        Equivalent to changing the settings on screen 3"""
         if 0 <= threshold <= 0.3:
             threshold = int(threshold*100)
             command = 0xB0 + threshold
@@ -157,7 +182,9 @@ class UM24C:
                 print(err)
         else:
             raise ValueError("Value must be between 0-0.3")
-    def SetBacklight(self, backlight):
+    def set_backlight(self, backlight):
+        """Changes the backlights settings to 'backlight' between 0 and 5
+        Equivalent to changing the settings on screen 7"""
         if 0 <= backlight <= 5:
             backlight = int(backlight)
             command = 0xD0 + backlight
@@ -168,7 +195,10 @@ class UM24C:
                 print(err)
         else:
             raise ValueError("Value must be between 0-5")
-    def SetScreensaver(self, screensaver):
+    def set_screensaver(self, screensaver):
+        """Changes the number of minutes until the screensaver blanks the screen
+        to 'screensaver' between 0 (never turn off) and up to 9 minutes.
+        Equivalent to changing the settings on screen 7"""
         if 0 <= screensaver <= 9:
             screensaver = int(screensaver)
             command = 0xE0 + screensaver
